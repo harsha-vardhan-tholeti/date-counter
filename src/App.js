55ed7011eb437c09b6d41cdfa822de8fb4c02233
Tholeti Harsha Vardhan <106816042+harsha-vardhan-tholeti@users.noreply.github.com>
2024-01-09 12:42:14 +0530

    removed unwanted code form app.js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,6 @@
 import { useState } from "react";
 import "./App.css";
 
-const month = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-
-const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
 function App() {
   const [step, setStep] = useState(0);
   const [counter, setCounter] = useState(0);
@@ -64,24 +47,6 @@ function App() {
         <span>Count: {step > 1 ? counter * step : counter}</span>
         <button onClick={increaseCounter}>+</button>
       </div>
-      {/* {counter === 0 ? (
-        <p>
-          Today is{" "}
-          {`${days[d.getDay()]} ${month[d.getMonth()]} ${
-            d.getDate() <= 9 ? "0" + d.getDate() : d.getDate()
-          } ${d.getFullYear()}`}
-        </p>
-      ) : (
-        <p>
-          {counter >= 1 && step > 1
-            ? `${step * counter} days from today is  ${t(
-                d.setDate(d.getDate() + counter * step)
-              )}`
-            : `${counter} days from today is ${t(
-                d.setDate(d.getDate() + counter)
-              )}`}
-        </p>
-      )} */}
 
       <p>
         {counter === 0 && `Today is ${t()}`}{" "}
